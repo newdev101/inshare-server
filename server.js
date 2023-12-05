f5ed7bb270da5422f4ce073d116d596a8d7a2aed
rajat jana <rajatjana2024@gmail.com>
2023-12-06 01:42:38 +0530

    required dotenv

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const cors = require('cors')
+require('dotenv').config();
+
 
 //middleware
 // app.use(bodyParser.json());
